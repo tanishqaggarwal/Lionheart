f61c08143be676461ca296a90beafa343c27c5cc
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2024-12-20 02:01:38 -0800

    Initial equations of motion

--- tanishq_dev/dynamics.cc
+++ tanishq_dev/dynamics.cc
@@ -1,17 +1,107 @@
 #include <array>
 #include <cmath>
 
-/// @brief Implements 6DOF dynamics for rover.
-class Rover
+#include "tanishq_dev/linalg.h"
+
+namespace Lionheart
 {
-private:
+
+/// @brief Implements 6DOF dynamics for rover.
+template<typename T>
+struct RoverT
+{
+    /// Density of seawater in the operating region
+    static constexpr T water_density = 1035.0; // kg / m^3
+
+    /// Inertial acceleration due to gravity
+    static constexpr Vector<3> accel_gravity {0, 0, 9.81}; // m/s^2
+
     // Internal state of system
+    template<typename Quantity>
+    struct integrated_state_t
+    {
+        Quantity value;
+        Quantity derivative;
+    };
 
-    /// Depth of the CG.
-    double cg_depth;
+    /// Position and velocity of CG, inertial frame.
+    integrated_state_t<Vector3<T>> position;
+    integrated_state_t<Vector3<T>> velocity;
 
-    /// Positions of thrust vectors
-    
+    /// Angular velocity of body w.r.t. CG, body frame.
+    integrated_state_t<Vector3<T>> angvel;
 
-public:
+    /// DCM translating vectors from body frame to inertial frame.
+    integrated_state_t<Matrix3<T>> attitude;
+
+    /// Total mass of rover (accounting for internal water storage as well.)
+    T mass = T{0.0};
+
+    /// Total dry volume of rover.
+    T volume = T{0.0};
+
+    /// Moment of inertia (and its inverse) in body frame.
+    Matrix3<T> moi;
+    Matrix3<T> moi_inv;
+
+    /// Location of center of buoyancy relative to CG in body frame.
+    Vector3<T> cb;
+
+    /// Positions and directions of thrust vectors, in body frame.
+    static constexpr size_t N_THRUSTERS = 5;
+    std::array<Vector3<T>, N_THRUSTERS> thrust_positions;
+    std::array<Vector3<T>, N_THRUSTERS> thrust_vectors;
+
+    std::tuple<Matrix3<T>, T> added_mass() const
+    {
+        // Compute the "effective" mass and MOI due to motion through water.
+        // TODO
+        // Assumptions:
+        // - Water is inviscid and incompressible
+        // - Make some assumptions about body geometry
+
+        return std::make_tuple(Matrix3<T>, T{0.0});
+    }
+
+    void update(const std::array<T, N_THRUSTERS>& thrusts)
+    {
+        // Get "added mass" effects.
+        const auto [m_added, moi_added] = added_mass();
+
+        // Get net thrust force in body frame.
+        Vector3<T> net_thrust_force;
+        for(size_t i = 0; i < N_THRUSTERS; i++)
+        {
+            net_thrust_force += thrust_vectors[i] * thrusts[i];
+        }
+
+        // Get net thrust torque in body frame.
+        Vector3<T> net_thrust_torque;
+        for(size_t i = 0; i < N_THRUSTERS; i++)
+        {
+            net_thrust_torque += thrust_positions[i].cross(thrust_vectors[i] * thrusts[i]);
+        }
+
+        // Get net buoyancy
+        Vector3<T> buoyancy_force = accel_gravity * water_density * volume * Vector3<T>{T{0}, T{0}, T{1}}; // Inertial frame
+        Vector3<T> buoyancy_torque = cb.cross(attitude.value.inverse() * buoyancy_force); // Body frame
+
+        // TODO compute drag force, current disturbances
+
+        // Aggregate torques and forces
+        Vector3<T> net_torque_body = buoyancy_torque + net_thrust_torque;
+        Vector3<T> net_force_inertial = buoyancy_force + attitude.value * net_thrust_force;
+        
+        // Compute equations of motion
+        position.derivative = velocity.value;
+        velocity.derivative = net_force_inertial / (mass + m_added);
+        angvel.derivative = moi_inv * ( net_torque_body - angvel.value.cross(moi * angvel.value) );
+
+        const auto& w = angvel.value;
+        attitude.derivative = attitude.value * Matrix3<T>{{0, -w.z, w.y}, {w.z, 0, -w.x}, {-w.y, w.x, 0}};
+    }
 };
+
+using Rover = RoverT<double>;
+
+} // end namespace Lionheart
--- tanishq_dev/linalg.h
+++ tanishq_dev/linalg.h
@@ -13,14 +13,24 @@ namespace Lionheart
 template<typename T>
 struct Vector3
 {
-    T x = 0.0;
-    T y = 0.0;
-    T z = 0.0;
+    T x = T{0.0};
+    T y = T{0.0};
+    T z = T{0.0};
 
     [[nodiscard]] T dot(const Vector3<T>& other) const
     {
         return this->x * other.x + this->y * other.y + this->z * other.z;
     }
+
+    [[nodiscard]] Vector3<T> cross(const Vector3<T>& other) const
+    {
+        Vector3 result;
+        result.x = this->y * other.z - other.y * this->z;
+        result.y = this->z * other.x - other.z * this->x;
+        result.z = this->x * other.y - other.x * this->y;
+        return result;
+    }
+
     [[nodiscard]] T norm() const
     {
         return std::sqrt(x*x + y*y + z*z);
@@ -37,26 +47,145 @@ struct Vector3
         *this = *this - other;
         return *this;
     }
+
+    void clear()
+    {
+        x = T{0.0};
+        y = T{0.0};
+        z = T{0.0};
+    }
 };
 
-Vector3 operator+(const Vector3& v1, const Vector3& v2)
+template<typename T>
+Vector3<T> operator+(const Vector3<T>& v1, const Vector3<T>& v2)
 {
     return {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
 }
 
-Vector3 operator-(const Vector3& v1, const Vector3& v2)
+template<typename T>
+Vector3<T> operator-(const Vector3<T>& v1, const Vector3<T>& v2)
 {
     return {v1.x - v2.x, v1.y - v2.y, v1.z - v2.z};
 }
 
-Vector3 operator*(const Vector3& v, const double c)
+template<typename T>
+Vector3<T> operator*(const Vector3<T>& v, const T c)
 {
     return {v.x * c, v.y * c, v.z * c};
 }
 
-Vector3 operator/(const Vector3& v, const double c)
+template<typename T>
+Vector3<T> operator*(const T c, const Vector3<T>& v)
+{
+    return v * c;
+}
+
+template<typename T>
+Vector3<T> operator/(const Vector3<T>& v, const T c)
 {
     return {v.x / c, v.y / c, v.z / c};
+}
+
+template<typename T>
+struct Matrix3
+{
+    Vector3<T> r1;
+    Vector3<T> r2;
+    Vector3<T> r3;
+
+    Matrix3 operator+=(const Matrix3& other)
+    {
+        *this = *this + other;
+        return *this;
+    }
+
+    Matrix3 operator-=(const Matrix3& other)
+    {
+        *this = *this - other;
+        return *this;
+    }
+
+    [[nodiscard]] Matrix3 inverse() const
+    {
+        // TODO
+        return Matrix3 {};
+    }
+
+    [[nodiscard]] Vector3<T> c1() const
+    {
+        return {r1.x, r2.x, r3.x};
+    }
+    [[nodiscard]] Vector3<T> c2() const
+    {
+        return {r1.y, r2.y, r3.y};
+    }
+    [[nodiscard]] Vector3<T> c3() const
+    {
+        return {r1.z, r2.z, r3.z};
+    }
+
+    void clear()
+    {
+        r1.clear();
+        r2.clear();
+        r3.clear();
+    }
+};
+
+template<typename T>
+Matrix3<T> operator+(const Matrix3<T>& m1, const Matrix3<T>& m2)
+{
+    return {m1.r1 + m2.r1, m1.r2 + m2.r2, m1.r3 + m2.r3};
+}
+
+template<typename T>
+Matrix3<T> operator-(const Matrix3<T>& m1, const Matrix3<T>& m2)
+{
+    return {m1.r1 - m2.r1, m1.r2 - m2.r2, m1.r3 - m2.r3};
+}
+
+template<typename T>
+Matrix3<T> operator*(const Matrix3<T>& m1, const Matrix3<T>& m2)
+{
+    Matrix3<T> result;
+    result.r1.x = m1.r1.dot(m2.c1());
+    result.r1.y = m1.r1.dot(m2.c2());
+    result.r1.z = m1.r1.dot(m2.c3());
+    result.r2.x = m1.r2.dot(m2.c1());
+    result.r2.y = m1.r2.dot(m2.c2());
+    result.r2.z = m1.r2.dot(m2.c3());
+    result.r3.x = m1.r3.dot(m2.c1());
+    result.r3.y = m1.r3.dot(m2.c2());
+    result.r3.z = m1.r3.dot(m2.c3());
+    return result;
+}
+
+template<typename T>
+Vector3<T> operator*(const Matrix3<T>& m, const Vector3<T>& v)
+{
+    Vector3<T> result;
+    result.x = m.r1.dot(v);
+    result.y = m.r2.dot(v);
+    result.z = m.r3.dot(v);
+    return result;
+}
+
+template<typename T>
+Matrix3<T> operator*(const Matrix3<T>& m, const T c)
+{
+    return {m.r1 * c, m.r2 * c, m.r3 * c};
+}
+
+template<typename T>
+Matrix3<T> operator*(const T c, const Matrix3<T>& m)
+{
+    return m * c;
+}
+
+template<typename T>
+Matrix3<T> operator/(const Matrix3<T>& m, const T c)
+{
+    return {m.r1 / c, m.r2 / c, m.r3 / c};
 }
 
 } // namespace Lionheart

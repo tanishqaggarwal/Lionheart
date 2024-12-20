fc68e7d0459731785278e450bea45955e900d4ad
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2024-12-20 02:25:38 -0800

    Matrix inversion implemented

--- tanishq_dev/linalg.h
+++ tanishq_dev/linalg.h
@@ -17,12 +17,12 @@ struct Vector3
     T y = T{0.0};
     T z = T{0.0};
 
-    [[nodiscard]] T dot(const Vector3<T>& other) const
+    [[nodiscard]] T dot(const Vector3<T>& other) const noexcept
     {
         return this->x * other.x + this->y * other.y + this->z * other.z;
     }
 
-    [[nodiscard]] Vector3<T> cross(const Vector3<T>& other) const
+    [[nodiscard]] Vector3<T> cross(const Vector3<T>& other) const noexcept
     {
         Vector3 result;
         result.x = this->y * other.z - other.y * this->z;
@@ -31,57 +31,79 @@ struct Vector3
         return result;
     }
 
-    [[nodiscard]] T norm() const
+    [[nodiscard]] T norm() const noexcept
     {
         return std::sqrt(x*x + y*y + z*z);
     }
 
-    Vector3 operator+=(const Vector3& other)
+    Vector3 operator+=(const Vector3& other) noexcept
     {
         *this = *this + other;
         return *this;
     }
 
-    Vector3 operator-=(const Vector3& other)
+    Vector3 operator-=(const Vector3& other) noexcept
     {
         *this = *this - other;
         return *this;
     }
 
-    void clear()
+    void clear() noexcept
     {
         x = T{0.0};
         y = T{0.0};
         z = T{0.0};
     }
+
+    [[nodiscard]] T& operator()(size_t i)
+    {
+        switch(i)
+        {
+            case 0: return x;
+            case 1: return y;
+            case 2: return z;
+            default: throw std::runtime_error{"invalid vector access"};
+        }
+    }
+
+    [[nodiscard]] const T& operator()(size_t i) const
+    {
+        switch(i)
+        {
+            case 0: return x;
+            case 1: return y;
+            case 2: return z;
+            default: throw std::runtime_error{"invalid vector access"};
+        }
+    }
 };
 
 template<typename T>
-Vector3<T> operator+(const Vector3<T>& v1, const Vector3<T>& v2)
+Vector3<T> operator+(const Vector3<T>& v1, const Vector3<T>& v2) noexcept
 {
     return {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
 }
 
 template<typename T>
-Vector3<T> operator-(const Vector3<T>& v1, const Vector3<T>& v2)
+Vector3<T> operator-(const Vector3<T>& v1, const Vector3<T>& v2) noexcept
 {
     return {v1.x - v2.x, v1.y - v2.y, v1.z - v2.z};
 }
 
 template<typename T>
-Vector3<T> operator*(const Vector3<T>& v, const T c)
+Vector3<T> operator*(const Vector3<T>& v, const T c) noexcept
 {
     return {v.x * c, v.y * c, v.z * c};
 }
 
 template<typename T>
-Vector3<T> operator*(const T c, const Vector3<T>& v)
+Vector3<T> operator*(const T c, const Vector3<T>& v) noexcept
 {
     return v * c;
 }
 
 template<typename T>
-Vector3<T> operator/(const Vector3<T>& v, const T c)
+Vector3<T> operator/(const Vector3<T>& v, const T c) noexcept
 {
     return {v.x / c, v.y / c, v.z / c};
 }
@@ -93,59 +115,102 @@ struct Matrix3
     Vector3<T> r2;
     Vector3<T> r3;
 
-    Matrix3 operator+=(const Matrix3& other)
+    Matrix3 operator+=(const Matrix3& other) noexcept
     {
         *this = *this + other;
         return *this;
     }
 
-    Matrix3 operator-=(const Matrix3& other)
+    Matrix3 operator-=(const Matrix3& other) noexcept
     {
         *this = *this - other;
         return *this;
     }
 
-    [[nodiscard]] Matrix3 inverse() const
+    [[nodiscard]] T determinant() const noexcept
     {
-        // TODO
-        return Matrix3 {};
+        const auto& m = *this;
+        return m.r1.x * (m.r2.y * m.r3.z - m.r3.y * m.r2.z) -
+             m.r1.y * (m.r2.x * m.r3.z - m.r2.z * m.r3.x) +
+             m.r1.z * (m.r2.x * m.r3.y - m.r2.y * m.r3.x);
     }
 
-    [[nodiscard]] Vector3<T> c1() const
+    [[nodiscard]] Matrix3 inverse() const noexcept
+    {
+        double invdet = 1 / determinant();
+
+        const auto& m = *this;
+        Matrix3 minv;
+        minv.r1.x = (m.r2.y * m.r3.z - m.r3.y * m.r2.z);
+        minv.r1.y = (m.r1.z * m.r3.y - m.r1.y * m.r3.z);
+        minv.r1.z = (m.r1.y * m.r2.z - m.r1.z * m.r2.y);
+        minv.r2.x = (m.r2.z * m.r3.x - m.r2.x * m.r3.z);
+        minv.r2.y = (m.r1.x * m.r3.z - m.r1.z * m.r3.x);
+        minv.r2.z = (m.r2.x * m.r1.z - m.r1.x * m.r2.z);
+        minv.r3.x = (m.r2.x * m.r3.y - m.r3.x * m.r2.y);
+        minv.r3.y = (m.r3.x * m.r1.y - m.r1.x * m.r3.y);
+        minv.r3.z = (m.r1.x * m.r2.y - m.r2.x * m.r1.y);
+
+        return minv * invdet;
+    }
+
+    [[nodiscard]] Vector3<T> c1() const noexcept
     {
         return {r1.x, r2.x, r3.x};
     }
-    [[nodiscard]] Vector3<T> c2() const
+    [[nodiscard]] Vector3<T> c2() const noexcept
     {
         return {r1.y, r2.y, r3.y};
     }
-    [[nodiscard]] Vector3<T> c3() const
+    [[nodiscard]] Vector3<T> c3() const noexcept
     {
         return {r1.z, r2.z, r3.z};
     }
 
-    void clear()
+    void clear() noexcept
     {
         r1.clear();
         r2.clear();
         r3.clear();
     }
+
+    [[nodiscard]] T& operator()(size_t i, size_t j)
+    {
+        switch(i)
+        {
+            case 0: return r1(j);
+            case 1: return r2(j);
+            case 2: return r3(j);
+            default: throw std::runtime_error{"invalid matrix access"};
+        }
+    }
+
+    [[nodiscard]] const T& operator()(size_t i, size_t j) const
+    {
+        switch(i)
+        {
+            case 0: return r1(j);
+            case 1: return r2(j);
+            case 2: return r3(j);
+            default: throw std::runtime_error{"invalid matrix access"};
+        }
+    }
 };
 
 template<typename T>
-Matrix3<T> operator+(const Matrix3<T>& m1, const Matrix3<T>& m2)
+Matrix3<T> operator+(const Matrix3<T>& m1, const Matrix3<T>& m2) noexcept
 {
     return {m1.r1 + m2.r1, m1.r2 + m2.r2, m1.r3 + m2.r3};
 }
 
 template<typename T>
-Matrix3<T> operator-(const Matrix3<T>& m1, const Matrix3<T>& m2)
+Matrix3<T> operator-(const Matrix3<T>& m1, const Matrix3<T>& m2) noexcept
 {
     return {m1.r1 - m2.r1, m1.r2 - m2.r2, m1.r3 - m2.r3};
 }
 
 template<typename T>
-Matrix3<T> operator*(const Matrix3<T>& m1, const Matrix3<T>& m2)
+Matrix3<T> operator*(const Matrix3<T>& m1, const Matrix3<T>& m2) noexcept
 {
     Matrix3<T> result;
     result.r1.x = m1.r1.dot(m2.c1());
@@ -161,7 +226,7 @@ Matrix3<T> operator*(const Matrix3<T>& m1, const Matrix3<T>& m2)
 }
 
 template<typename T>
-Vector3<T> operator*(const Matrix3<T>& m, const Vector3<T>& v)
+Vector3<T> operator*(const Matrix3<T>& m, const Vector3<T>& v) noexcept
 {
     Vector3<T> result;
     result.x = m.r1.dot(v);
@@ -171,19 +236,19 @@ Vector3<T> operator*(const Matrix3<T>& m, const Vector3<T>& v)
 }
 
 template<typename T>
-Matrix3<T> operator*(const Matrix3<T>& m, const T c)
+Matrix3<T> operator*(const Matrix3<T>& m, const T c) noexcept
 {
     return {m.r1 * c, m.r2 * c, m.r3 * c};
 }
 
 template<typename T>
-Matrix3<T> operator*(const T c, const Matrix3<T>& m)
+Matrix3<T> operator*(const T c, const Matrix3<T>& m) noexcept
 {
     return m * c;
 }
 
 template<typename T>
-Matrix3<T> operator/(const Matrix3<T>& m, const T c)
+Matrix3<T> operator/(const Matrix3<T>& m, const T c) noexcept
 {
     return {m.r1 / c, m.r2 / c, m.r3 / c};
 }

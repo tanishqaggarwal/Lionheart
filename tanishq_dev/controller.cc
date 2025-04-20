6074c36411d7c9690137c830deed2afec84b113a
Paul Joo <pauljoold@gmail.com>
2025-04-20 10:25:43 -0700

    Add in some quaternion utils

--- tanishq_dev/controller.cc
+++ tanishq_dev/controller.cc
@@ -1,21 +1,17 @@
 #include "tanishq_dev/controller.h"
 
-namespace Lionheart
-{
+namespace Lionheart {
 
 template <typename T>
 AttitudeRegulationControllerT<T>::AttitudeRegulationControllerT(T kp, T kd)
-    : kp(kp),
-      kd(kd)
-{}
+    : kp(kp), kd(kd) {}
 
 template <typename T>
-Vector3<T> AttitudeRegulationControllerT<T>::update(
-    const Matrix3<T>& attitude_sns,
-    const Matrix3<T>& attitude_cmd,
-    const Vector3<T>& angvel_sns)
-{
+Vector3<T>
+AttitudeRegulationControllerT<T>::update(const Matrix3<T> &attitude_sns,
+                                         const Matrix3<T> &attitude_cmd,
+                                         const Vector3<T> &angvel_sns) {
     return Vector3<T>{};
 }
 
-}
+} // namespace Lionheart
--- tanishq_dev/controller.h
+++ tanishq_dev/controller.h
@@ -1,17 +1,13 @@
 #include "tanishq_dev/linalg.h"
 
-namespace Lionheart
-{
-template<typename T>
-struct AttitudeRegulationControllerT
-{
+namespace Lionheart {
+template <typename T> struct AttitudeRegulationControllerT {
     AttitudeRegulationControllerT(T kp, T kd);
-    Vector3<T> update(
-        const Matrix3<T>& attitude_sns,
-        const Matrix3<T>& attitude_cmd,
-        const Vector3<T>& angvel_sns);
+    Vector3<T> update(const Matrix3<T> &attitude_sns,
+                      const Matrix3<T> &attitude_cmd,
+                      const Vector3<T> &angvel_sns);
 
     T kp;
     T kd;
 };
-}
+} // namespace Lionheart

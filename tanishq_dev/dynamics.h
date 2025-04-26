d70cedc7335433a043ba4c54e8e25d12905423ce
Tanishq Aggarwal <tanishq@tanishqaggarwal.com>
2025-04-26 16:34:57 -0700

    Initial stab at visualizer

--- tanishq_dev/dynamics.h
+++ tanishq_dev/dynamics.h
@@ -8,13 +8,16 @@
 namespace Lionheart {
 
 /// @brief Implements 6DOF dynamics for rover.
-template <typename T> struct RoverT {
-    // Internal state of system
-    template <typename Quantity> struct integrated_state_t {
-        Quantity value;
-        Quantity derivative;
-    };
+template <typename T> class RoverT {
+private:
+  // Internal state of system
+  template <typename Quantity> struct integrated_state_t {
+    Quantity value;
+    Quantity derivative;
+  };
 
+public:
+  struct state_t {
     /// Position and velocity of CG, inertial frame.
     integrated_state_t<Vector3<T>> position;
     integrated_state_t<Vector3<T>> velocity;
@@ -25,86 +28,132 @@ template <typename T> struct RoverT {
     /// DCM translating vectors from body frame to inertial frame.
     integrated_state_t<Matrix3<T>> attitude;
 
-    struct config_t {
-        /// Density of seawater in the operating region
-        static constexpr T water_density{1035.0}; // kg / m^3
+    state_t(const Vector3<T> &initial_position,
+            const Vector3<T> &initial_velocity,
+            const Vector3<T> &initial_angular_velicity,
+            const Matrix3<T> &initial_attitude)
+        : position({.value = initial_position, .derivative = Vector3<T>()}),
+          velocity({.value = initial_velocity, .derivative = Vector3<T>()}),
+          angular_velocity(
+              {.value = initial_angular_velicity, .derivative = Vector3<T>()}),
+          attitude({.value = initial_attitude, .derivative = Matrix3<T>()}) {}
+  };
 
-        /// Inertial acceleration due to gravity
-        static constexpr Vector3<T> accel_gravity{0, 0, 9.81}; // m/s^2
+private:
+  state_t state;
 
-        /// Total mass of rover (accounting for internal water storage as well.)
-        T mass{0.0};
+public:
+  struct config_t {
+    /// Density of seawater in the operating region
+    static constexpr T water_density{1035.0}; // kg / m^3
 
-        /// Total dry volume of rover.
-        T volume{0.0};
+    /// Inertial acceleration due to gravity
+    static constexpr Vector3<T> accel_gravity{0, 0, 9.81}; // m/s^2
 
-        /// Moment of inertia (and its inverse) in body frame.
-        Matrix3<T> moi;
-        Matrix3<T> moi_inv;
+    /// Total mass of rover (accounting for internal water storage as well.)
+    T mass{0.0};
 
-        /// Location of center of buoyancy relative to CG in body frame.
-        Vector3<T> cb;
+    /// Total dry volume of rover.
+    T volume{0.0};
 
-        /// Positions and directions of thrust vectors, in body frame.
-        static constexpr size_t N_THRUSTERS{5};
-        std::array<Vector3<T>, N_THRUSTERS> thrust_positions;
-        std::array<Vector3<T>, N_THRUSTERS> thrust_vectors;
-    } config;
+    /// Moment of inertia (and its inverse) in body frame.
+    Matrix3<T> moi;
+    Matrix3<T> moi_inv;
 
-    explicit RoverT(const config_t &config_) : config(config_) {}
+    /// Location of center of buoyancy relative to CG in body frame.
+    Vector3<T> cb;
 
-    // Compute the "effective" mass and MOI due to motion through water.
-    // TODO
-    // Assumptions:
-    // - Water is inviscid and incompressible
-    // - Make some assumptions about body geometry
-    std::tuple<Matrix3<T>, T> added_mass() const {
-        return std::make_tuple(Matrix3<T>{}, T{0.0});
+    /// Positions and directions of thrust vectors, in body frame.
+    static constexpr size_t N_THRUSTERS{5};
+    std::array<Vector3<T>, N_THRUSTERS> thrust_positions;
+    std::array<Vector3<T>, N_THRUSTERS> thrust_vectors;
+
+    /// Constructor
+    config_t(T mass_, T volume_, const Matrix3<T> &moi_, const Vector3<T> &cb_,
+             const std::array<Vector3<T>, N_THRUSTERS> &thrust_positions_,
+             const std::array<Vector3<T>, N_THRUSTERS> &thrust_vectors_)
+        : mass(mass_), volume(volume_), moi(moi_), moi_inv(moi_.inverse()),
+          cb(cb_), thrust_positions(thrust_positions_),
+          thrust_vectors(thrust_vectors_) {}
+  } config;
+
+  RoverT(const state_t &initial_state, const config_t &config_)
+      : state(initial_state), config(config_) {}
+
+  // Compute the "effective" mass and MOI due to motion through water.
+  // TODO
+  // Assumptions:
+  // - Water is inviscid and incompressible
+  // - Make some assumptions about body geometry
+  std::tuple<Matrix3<T>, T> added_mass() const {
+    return std::make_tuple(Matrix3<T>{}, T{0.0});
+  }
+
+  void update(const std::array<T, config_t::N_THRUSTERS> &thrusts) {
+    // Get "added mass" effects.
+    const auto [moi_added, m_added] = added_mass();
+
+    // Get net thrust force in body frame.
+    Vector3<T> net_thrust_force;
+    for (size_t i = 0; i < config_t::N_THRUSTERS; i++) {
+      net_thrust_force += config.thrust_vectors[i] * thrusts[i];
     }
 
-    void update(const std::array<T, config_t::N_THRUSTERS> &thrusts) {
-        // Get "added mass" effects.
-        const auto [moi_added, m_added] = added_mass();
-
-        // Get net thrust force in body frame.
-        Vector3<T> net_thrust_force;
-        for (size_t i = 0; i < config_t::N_THRUSTERS; i++) {
-            net_thrust_force += config.thrust_vectors[i] * thrusts[i];
-        }
-
-        // Get net thrust torque in body frame.
-        Vector3<T> net_thrust_torque;
-        for (size_t i = 0; i < config_t::N_THRUSTERS; i++) {
-            net_thrust_torque += config.thrust_positions[i].cross(
-                config.thrust_vectors[i] * thrusts[i]);
-        }
-
-        // Get net buoyancy
-        Vector3<T> buoyancy_force = -config_t::water_density * config.volume *
-                                    config_t::accel_gravity; // Inertial frame
-        Vector3<T> buoyancy_torque = config.cb.cross(
-            attitude.value.inverse() * buoyancy_force); // Body frame
-
-        // TODO compute drag force, current disturbances
-
-        // Aggregate torques and forces
-        Vector3<T> net_torque_body = buoyancy_torque + net_thrust_torque;
-        Vector3<T> net_force_inertial =
-            buoyancy_force + attitude.value * net_thrust_force;
-
-        // Compute equations of motion
-        position.derivative = velocity.value;
-        velocity.derivative = net_force_inertial / (config.mass + m_added);
-        angular_velocity.derivative =
-            config.moi_inv *
-            (net_torque_body -
-             angular_velocity.value.cross(config.moi * angular_velocity.value));
-
-        const auto &w = angular_velocity.value;
-        attitude.derivative =
-            attitude.value *
-            Matrix3<T>{{0, -w.z, w.y}, {w.z, 0, -w.x}, {-w.y, w.x, 0}};
+    // Get net thrust torque in body frame.
+    Vector3<T> net_thrust_torque;
+    for (size_t i = 0; i < config_t::N_THRUSTERS; i++) {
+      net_thrust_torque += config.thrust_positions[i].cross(
+          config.thrust_vectors[i] * thrusts[i]);
     }
+
+    // Get net buoyancy
+    Vector3<T> buoyancy_force = -config_t::water_density * config.volume *
+                                config_t::accel_gravity; // Inertial frame
+    Vector3<T> buoyancy_torque = config.cb.cross(
+        state.attitude.value.inverse() * buoyancy_force); // Body frame
+
+    // TODO compute drag force, current disturbances
+
+    // Aggregate torques and forces
+    Vector3<T> net_torque_body = buoyancy_torque + net_thrust_torque;
+    Vector3<T> net_force_inertial =
+        buoyancy_force + state.attitude.value * net_thrust_force;
+
+    // Compute equations of motion
+    state.position.derivative = state.velocity.value;
+    state.velocity.derivative = net_force_inertial / (config.mass + m_added);
+    state.angular_velocity.derivative =
+        config.moi_inv *
+        (net_torque_body - state.angular_velocity.value.cross(
+                               config.moi * state.angular_velocity.value));
+
+    const auto &w = state.angular_velocity.value;
+    state.attitude.derivative =
+        state.attitude.value *
+        Matrix3<T>{{0, -w.z, w.y}, {w.z, 0, -w.x}, {-w.y, w.x, 0}};
+  }
+
+  template <typename IntegratorFn>
+  void integrate(T dt, IntegratorFn integrator) {
+    state.position.value =
+        integrator(state.position.value, state.position.derivative, dt);
+    state.velocity.value =
+        integrator(state.velocity.value, state.velocity.derivative, dt);
+    state.angular_velocity.value = integrator(
+        state.angular_velocity.value, state.angular_velocity.derivative, dt);
+    state.attitude.value =
+        integrator(state.attitude.value, state.attitude.derivative, dt);
+  }
+
+  void integrate_euler(T dt) {
+    integrate(dt, [](const auto &state, const auto &derivative, T dt) {
+      return state + derivative * dt;
+    });
+  }
+
+  Vector3<T> get_position() const { return state.position.value; }
+
+  Matrix3<T> get_attitude() const { return state.attitude.value; }
 };
 
 using Rover = RoverT<double>;
--- tanishq_dev/dynamics_test.py
+++ tanishq_dev/dynamics_test.py
@@ -1,1 +1,88 @@
-from tanishq_dev.dynamics_py import Rover
+import tanishq_dev.dynamics_py as lionheart
+from typing import Callable
+import numpy as np
+import vedo
+
+class Simulation:
+    def __init__(
+        self,
+        initial_state: lionheart.RoverState,
+        config: lionheart.RoverConfig,
+        box: vedo.Box,
+    ) -> None:
+        self.rover = lionheart.Rover(initial_state, config)
+        self.plotter = vedo.Plotter(interactive=False, axes=1)
+        self.box = box
+        self.plotter += self.box
+
+    @property
+    def position(self) -> np.ndarray:
+        pos = self.rover.get_position()
+        return pos.to_numpy()
+
+    @property
+    def attitude(self) -> np.ndarray:
+        att = self.rover.get_attitude()
+        return att.to_numpy()
+
+    def _step(
+        self, t: float, dt: float, thrust_fn: Callable[float, list[float]]
+    ) -> None:
+        # Update physics
+        self.rover.update(thrust_fn(t))
+        self.rover.integrate_euler(dt)
+
+        # Update visualization
+        self.box.pos(self.position)
+        transform = np.eye(4)
+        transform[:3, :3] = self.attitude
+        transform[:3, 3] = self.position
+        self.box.apply_transform(transform)
+        self.plotter.render()
+
+    def run(
+        self, tf: float, dt: float, thrust_fn: Callable[float, list[float]]
+    ) -> None:
+        n_timesteps = int(tf // dt)
+        for i in range(n_timesteps):
+            self._step(i * dt, dt, thrust_fn)
+
+    def stop(self):
+        self.plotter.interactive().close()
+
+
+config = lionheart.RoverConfig(
+    mass=1.0,
+    volume=1.0,
+    moi=lionheart.Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]]),
+    cb=lionheart.Vector(0, 0, 0),
+    thrust_positions=[
+        lionheart.Vector(0, 0, 0),
+        lionheart.Vector(0, 0, 0),
+        lionheart.Vector(0, 0, 0),
+        lionheart.Vector(0, 0, 0),
+        lionheart.Vector(0, 0, 0),
+    ],
+    thrust_vectors=[
+        lionheart.Vector(1, 0, 0),
+        lionheart.Vector(1, 0, 0),
+        lionheart.Vector(1, 0, 0),
+        lionheart.Vector(1, 0, 0),
+        lionheart.Vector(1, 0, 0),
+    ],
+)
+
+initial_position = (0, 0, 0)
+initial_state = lionheart.RoverState(
+    position=lionheart.Vector(*initial_position),
+    velocity=lionheart.Vector(0, 0, 0),
+    angular_velocity=lionheart.Vector(0, 0, 0),
+    attitude=lionheart.Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]]),
+)
+rover_body = vedo.Box(pos=initial_position, length=1, width=1, height=1, c="blue")
+sim = Simulation(initial_state, config, rover_body)
+
+thrust_fn = lambda t: [0, 0, 0, 0, 0]
+sim.run(10, 0.01, thrust_fn)
+input("Press Enter to exit.")
+sim.stop()

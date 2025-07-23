3d9b4a3da27241d5daecdfd65f105c65612fc78c
Paul Joo <pauljoold@gmail.com>
2025-07-23 00:25:41 -0700

    Get commanding working

--- paul_dev/sensor/src/main.cpp
+++ paul_dev/sensor/src/main.cpp
@@ -24,31 +24,33 @@ unsigned long uptime_ms = 0;
 // 20 Hz
 const float CONTROL_CYCLE_TIME_ms = 50.0;
 
-Adafruit_LSM303_Accel_Unified accel;
-Adafruit_LSM303_Mag_Unified mag;
-Adafruit_9DOF dof;
+// Adafruit_LSM303_Accel_Unified accel;
+// Adafruit_LSM303_Mag_Unified mag;
+// Adafruit_9DOF dof;
 
-bool init_sensors_done = false;
-void init_sensors() {
+// bool init_sensors_done = false;
+// void init_sensors() {
 
-    Serial.println("Setting up Sensors...");
+//     Serial.println("Setting up Sensors...");
 
-    if (!accel.begin()) {
-        /* There was a problem detecting the LSM303 ... check your connections
-         */
-        Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
-        return;
-    }
-    if (!mag.begin()) {
-        /* There was a problem detecting the LSM303 ... check your connections
-         */
-        Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
-        return;
-    }
-    init_sensors_done = true;
+//     if (!accel.begin()) {
+//         /* There was a problem detecting the LSM303 ... check your
+//         connections
+//          */
+//         Serial.println(F("Ooops, no LSM303 detected ... Check your
+//         wiring!")); return;
+//     }
+//     if (!mag.begin()) {
+//         /* There was a problem detecting the LSM303 ... check your
+//         connections
+//          */
+//         Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+//         return;
+//     }
+//     init_sensors_done = true;
 
-    Serial.println("Sensor setup complete.");
-}
+//     Serial.println("Sensor setup complete.");
+// }
 
 #define NET_PORT 3000
 bool init_ethernet_done = false;
@@ -81,11 +83,13 @@ void init_ethernet() {
     if (Ethernet.hardwareStatus() == EthernetNoHardware) {
         Serial.println("Ethernet shield was not found.  Sorry, can't run "
                        "without hardware. :(");
+        return;
     }
     Serial.println("Checking ethernet link status.");
     if (Ethernet.linkStatus() == LinkOFF) {
         // W5100 will expect link status UNKNOWN even if the cable is connected.
         Serial.println("Ethernet cable is not connected.");
+        return;
     }
 
     net_driver.begin(NET_PORT);
@@ -98,30 +102,49 @@ void init_ethernet() {
     init_ethernet_done = true;
 }
 
+IPAddress control_panel_addr;
 int control_panel_port = -1;
 void read_command() {
-    Serial.println("Reading command from control panel...");
-    unsigned char net_buffer[512];
-    if (control_panel_port == -1) {
-        net_driver.parsePacket();
-        int bytes_read = net_driver.read(net_buffer, sizeof(net_buffer));
-        if (bytes_read > 0) {
-            Serial.print("Received command: ");
-            Serial.write(net_buffer, bytes_read);
-            Serial.println();
+    unsigned char net_buffer[64];
+    int packet_size = net_driver.parsePacket();
+    if (packet_size == 0) {
+        return;
+    }
+    if (packet_size > sizeof(net_buffer)) {
+        Serial.println("Error: Received packet too large. Emptying.");
+        while (net_driver.read(net_buffer, sizeof(net_buffer)) != -1) {
+            // Keep reading until the buffer is empty
         }
+        return;
+    }
+
+    if (control_panel_port == -1) {
+        int bytes_read = net_driver.read(net_buffer, sizeof(net_buffer));
         if (bytes_read != 0 &&
             strncmp(reinterpret_cast<const char *>(net_buffer), "connect", 7) ==
                 0) {
             control_panel_port = net_driver.remotePort();
-            net_driver.beginPacket(net_driver.remoteIP(), control_panel_port);
-            net_driver.write("Connected to control panel.");
+            control_panel_addr = net_driver.remoteIP();
+            net_driver.beginPacket(control_panel_addr, control_panel_port);
+            net_driver.write("Connection Success!\n");
             net_driver.endPacket();
-            Serial.print("Connected to control panel at port: ");
+            Serial.print("Connection Success! Control panel port: ");
             Serial.println(control_panel_port);
         }
         return;
     }
+
+    int bytes_read = net_driver.read(net_buffer, sizeof(net_buffer));
+    bool success = process_command(reinterpret_cast<const char *>(net_buffer),
+                                   bytes_read, state);
+
+    net_driver.beginPacket(control_panel_addr, control_panel_port);
+    if (success) {
+        net_driver.write("Command Success!\n");
+    } else {
+        net_driver.write("Command Failed!\n");
+    }
+    net_driver.endPacket();
 }
 
 #define ENA_1_PIN 6
@@ -154,7 +177,7 @@ void setup() {
     Serial.begin(115200);
     Serial.println("Setting Up...");
 
-    init_sensors();
+    // init_sensors();
     init_ethernet();
     initMotorController();
 
@@ -163,42 +186,41 @@ void setup() {
 
 void loop() {
     loop_start_ms = static_cast<float>(micros()) / 1000.0;
-
-    delay(1000);
     // if (loop_start_ms - loop_end_ms < CONTROL_CYCLE_TIME_ms) {
-    //     Serial.println("Hello");
     //     delay(1); // Wait 1 millisecond in between polls.
     //     return;
     // }
+    delay(1000);
 
     if (init_ethernet_done) {
         read_command();
     }
 
-    if (init_sensors_done) {
-        // Do work here:
-        sensors_event_t accel_event;
-        sensors_event_t mag_event;
-        sensors_vec_t orientation;
+    // if (init_sensors_done) {
+    //     // Do work here:
+    //     sensors_event_t accel_event;
+    //     sensors_event_t mag_event;
+    //     sensors_vec_t orientation;
 
-        accel.getEvent(&accel_event);
-        if (dof.accelGetOrientation(&accel_event, &orientation)) {
-            Telemetry::imu.roll = orientation.roll;
-            Telemetry::imu.pitch = orientation.pitch;
-        }
+    //     accel.getEvent(&accel_event);
+    //     if (dof.accelGetOrientation(&accel_event, &orientation)) {
+    //         Telemetry::imu.roll = orientation.roll;
+    //         Telemetry::imu.pitch = orientation.pitch;
+    //     }
 
-        /* Calculate the heading using the magnetometer */
-        mag.getEvent(&mag_event);
-        if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation)) {
-            Telemetry::imu.heading = orientation.heading;
-        }
+    //     /* Calculate the heading using the magnetometer */
+    //     mag.getEvent(&mag_event);
+    //     if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation)) {
+    //         Telemetry::imu.heading = orientation.heading;
+    //     }
 
-        Telemetry::imu.uptime_ms = static_cast<uint32_t>(millis());
-        Telemetry::imu.loop_time_ms = loop_end_ms - loop_start_ms;
+    //     Telemetry::imu.uptime_ms = static_cast<uint32_t>(millis());
+    //     Telemetry::imu.loop_time_ms = loop_end_ms - loop_start_ms;
 
-        Telemetry::imu.writePacket();
-        Serial.write(Telemetry::imu.packet, Telemetry::imu.PACKET_SIZE_BYTES);
-    }
+    //     Telemetry::imu.writePacket();
+    //     Serial.write(Telemetry::imu.packet,
+    //     Telemetry::imu.PACKET_SIZE_BYTES);
+    // }
 
     loop_end_ms = static_cast<float>(micros()) / 1000.0;
 }
--- paul_dev/sensor/src/state.h
+++ paul_dev/sensor/src/state.h
@@ -1,23 +1,58 @@
 #pragma once
 
+#include <Arduino.h>
 #include <stdint.h>
+#include <string.h>
+
+#define FIELDS                                                                 \
+    FIELD(uint32_t, gps_time_nano_s_hi, 0)                                     \
+    FIELD(uint32_t, gps_time_nano_s_lo, 0)                                     \
+    FIELD(uint32_t, m1_speed, 0)                                               \
+    FIELD(bool, m1_en, 0)                                                      \
+    FIELD(bool, m1_in1, 0)                                                     \
+    FIELD(bool, m1_in2, 0)                                                     \
+    FIELD(bool, m1_gnd, 0)                                                     \
+    FIELD(uint32_t, m2_speed, 0)                                               \
+    FIELD(bool, m2_en, 0)                                                      \
+    FIELD(bool, m2_in1, 0)                                                     \
+    FIELD(bool, m2_in2, 0)                                                     \
+    FIELD(bool, m2_gnd, 0)
 
 struct state_t {
-    // GPS time in nanoseconds
-    uint32_t gps_time_nano_s_hi = 0;
-    uint32_t gps_time_nano_s_lo = 0;
-
-    // M1
-    uint32_t m1_speed = 0;
-    bool m1_en = 0;
-    bool m1_in1 = 0;
-    bool m1_in2 = 0;
-    bool m1_gnd = 0;
-
-    // M2
-    uint32_t m2_speed = 0;
-    bool m2_en = 0;
-    bool m2_in1 = 0;
-    bool m2_in2 = 0;
-    bool m2_gnd = 0;
+#define FIELD(type, name, default_value) type name = default_value;
+    FIELDS
+#undef FIELD
 };
+
+bool process_command(const char *command, int len, state_t &state) {
+    bool success = false;
+    char cmd_buffer[64];
+    memcpy(cmd_buffer, command, len);
+    // -1 to remove the trailing newline
+    cmd_buffer[len - 1] = '\0';
+    String s(cmd_buffer);
+    Serial.print("Command Received: ");
+    Serial.println(s);
+#define FIELD(type, name, default_value)                                       \
+    {                                                                          \
+        if (strncmp(cmd_buffer, #name, sizeof(#name) - 1) == 0) {              \
+            String input = String(&cmd_buffer[sizeof(#name)]);                 \
+            int val = input.toInt();                                           \
+            if (val == 0 && input != "0") {                                    \
+                return false;                                                  \
+            }                                                                  \
+            state.name = (type)val;                                            \
+            success = true;                                                    \
+            Serial.print("Success: Set ");                                     \
+            Serial.print(#name);                                               \
+            Serial.print(" to ");                                              \
+            Serial.println(state.name);                                        \
+        }                                                                      \
+    }
+    FIELDS
+#undef FIELD
+
+    return success;
+}
+
+#undef FIELDS

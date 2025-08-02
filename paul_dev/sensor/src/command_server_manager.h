744861d6a331c38d69f9e7d1c53d67750d1bf35f
Paul Joo <pauljoold@gmail.com>
2025-08-02 14:54:48 -0700

    Change how atoi is used

--- paul_dev/sensor/src/command_server_manager.h
+++ paul_dev/sensor/src/command_server_manager.h
@@ -133,37 +133,6 @@ struct CommandServerManager {
         return true;
     }
 
-    bool process_command(const char *command, int len, state_t &state) {
-        bool success = false;
-        char cmd_buffer[64];
-        memcpy(cmd_buffer, command, len);
-        // -1 to remove the trailing newline
-        cmd_buffer[len - 1] = '\0';
-        String s(cmd_buffer);
-        Serial.print("Command Received: ");
-        Serial.println(s);
-#define FIELD(type, name, default_value)                                       \
-    {                                                                          \
-        if (strncmp(cmd_buffer, #name, sizeof(#name) - 1) == 0) {              \
-            String input = String(&cmd_buffer[sizeof(#name)]);                 \
-            int val = input.toInt();                                           \
-            if (val == 0 && input != "0") {                                    \
-                return false;                                                  \
-            }                                                                  \
-            state.name = (type)val;                                            \
-            success = true;                                                    \
-            Serial.print("Success: Set ");                                     \
-            Serial.print(#name);                                               \
-            Serial.print(" to ");                                              \
-            Serial.println(state.name);                                        \
-        }                                                                      \
-    }
-        FIELDS
-#undef FIELD
-
-        return success;
-    }
-
     void read_command() {
         unsigned char net_buffer[64];
         int packet_size = net_driver.parsePacket();
--- paul_dev/sensor/src/state.h
+++ paul_dev/sensor/src/state.h
@@ -1,32 +1,61 @@
 #pragma once
 
+#ifndef __AVR__
+#include <cstdlib>
+#else
+#include <stdlib.h>
+#endif
+
 #include <stdint.h>
 #include <string.h>
 
-#define FIELDS                                                                 \
+#define READ_ONLY_FIELDS                                                       \
     FIELD(uint32_t, gps_time_nano_s_hi, 0)                                     \
     FIELD(uint32_t, gps_time_nano_s_lo, 0)                                     \
-    FIELD(uint32_t, m1_speed, 0)                                               \
-    FIELD(bool, m1_en, 0)                                                      \
     FIELD(bool, m1_in1, 0)                                                     \
     FIELD(bool, m1_in2, 0)                                                     \
-    FIELD(bool, m1_gnd, 0)                                                     \
-    FIELD(uint32_t, m2_speed, 0)                                               \
-    FIELD(bool, m2_en, 0)                                                      \
     FIELD(bool, m2_in1, 0)                                                     \
-    FIELD(bool, m2_in2, 0)                                                     \
-    FIELD(bool, m2_gnd, 0)
+    FIELD(bool, m2_in2, 0)
+
+#define READ_WRITE_FIELDS                                                      \
+    FIELD(uint32_t, m1_speed, 0)                                               \
+    FIELD(uint32_t, m2_speed, 0)
 
 struct state_t {
 #define FIELD(type, name, default_value) type name;
-    FIELDS
+    READ_WRITE_FIELDS
+    READ_ONLY_FIELDS
 #undef FIELD
 };
 
 void init_state(state_t &state) {
 #define FIELD(type, name, default_value) state.name = default_value;
-    FIELDS
+    READ_WRITE_FIELDS
+    READ_ONLY_FIELDS
 #undef FIELD
 }
 
-#undef FIELDS
+bool process_command(const char *command, int len, state_t &state) {
+    bool success = false;
+    char cmd_buffer[64];
+    memcpy(cmd_buffer, command, len);
+    // -1 to remove the trailing newline
+    cmd_buffer[len - 1] = '\0';
+#define FIELD(type, name, default_value)                                       \
+    {                                                                          \
+        if (strncmp(cmd_buffer, #name, sizeof(#name) - 1) == 0) {              \
+            int val = atoi(&cmd_buffer[sizeof(#name)]);                        \
+            if (val == 0 && cmd_buffer[sizeof(#name)] != '0') {                \
+                return false;                                                  \
+            }                                                                  \
+            state.name = (type)val;                                            \
+            success = true;                                                    \
+        }                                                                      \
+    }
+    READ_WRITE_FIELDS
+#undef FIELD
+
+    return success;
+}
+
+#undef READ_WRITE_FIELDS
